cc777d0c317dc610e250b70361449db5518c4d2b
matthewjmontalto <matthewjmontalto@gmail.com>
2019-04-17 12:32:07 -0400

    Sort and filter by task date
    - add functions that sort tasks from future to past
    - add functions that filter tasks and returns today's tasks

--- src/tasks/Tasks.js
+++ src/tasks/Tasks.js
@@ -36,15 +36,32 @@ class Tasks extends Component {
       return <NavLink to="/create-task">Add Task</NavLink>
     }
 
+    // at render, sort tasks object by date
+    const sortedTasks = this.state.tasks.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date)
+    })
+
+    const todaysTasks = sortedTasks.filter(task => {
+      // starting format is YYYY-MM-DD. Append 12:00 to force date to be today
+      const ignoreTimezone = `${task.date} 12:00`
+      // convert starting format to JS date formate
+      const taskDate = new Date(ignoreTimezone)
+      // get today's date
+      const today = new Date(Date.now())
+      // return new array with objects containing today's date
+      return taskDate.toDateString() === today.toDateString()
+    })
+
     return (
       <Fragment>
         <NavLink to="/create-task">Add Task</NavLink>
         <h2>Today&#39;s Tasks</h2>
         <ul>
-          { this.state.tasks.map(task => (
+          { todaysTasks.map(task => (
             <li key={task.id}>
               <Link to={'/tasks/' + task.id}>{task.title}</Link>
               <p>pomodoro rounds: {task.number_pomodoro_sessions}</p>
+              <p>date: {task.date}</p>
               <Timer
                 user={this.props.user}
                 task={task}
